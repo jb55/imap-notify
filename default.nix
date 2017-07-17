4c75b31a7731417b7fa9b5a41764b0a2f083d564
William Casarin <jb55@jb55.com>
2017-07-17 15:04:48 -0700

    nix config for easy install

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ stdenv, makeWrapper, nodejs }:
+
+stdenv.mkDerivation rec {
+  name = "imap-notify-${version}";
+  version = "0.1";
+
+  src = ./.;
+
+  makeFlags = "PREFIX=$(out)";
+
+  phases = ["installPhase"];
+
+  buildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/index.js $out/bin/imap-notify
+    wrapProgram $out/bin/imap-notify \
+      --prefix PATH : "${nodejs}/bin"
+  '';
+
+  meta = with stdenv.lib; {
+    description = "imap-notify";
+    homepage = "https://github.com/jb55/gmail-notify";
+    maintainers = with maintainers; [ jb55 ];
+    license = licenses.mit;
+  };
+}
